829f09ad6803e714e4f2586d206127ce0e765d11
Edward <edwardddpowers3@knights.ucf.edu>
2019-12-07 04:33:32 -0500

    new

---
+++ CountdownScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountdownScript : MonoBehaviour
+{
+    public float startingTime;
+
+    private Text textTime;
+    void Start()
+    {
+        textTime = GetComponent<Text>();
+    }
+    void Update()
+    {
+        startingTime -= Time.deltaTime;
+
+        if(startingTime < 0)
+        {
+            startingTime = 0;
+        }
+        textTime.text = "Time Remaining:" + Mathf.Round(startingTime);
+    }
+}
