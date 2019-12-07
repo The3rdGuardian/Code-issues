a1148a75405f05e12366e6c911e880e709d1a730
Edward <edwardddpowers3@knights.ucf.edu>
2019-12-06 20:05:38 -0500

    new

---
+++ EnemyMover.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMover : MonoBehaviour
+{
+
+    public Transform startMarker;
+    public Transform endMarker;
+
+    public float speed = 1.0F;
+
+    private float startTime;
+
+    private float journeyLength;
+
+    private bool facingRight = true;
+
+    void Start()
+    {
+        startTime = Time.time;
+
+        journeyLength = Vector2.Distance(startMarker.position, endMarker.position);
+    }
+
+    void Update()
+    {
+
+        float distCovered = (Time.time - startTime) * speed;
+
+        float fracJourney = distCovered / journeyLength;
+
+        transform.position = Vector2.Lerp(startMarker.position, endMarker.position, Mathf.PingPong(fracJourney, 1));
+
+        if (facingRight == false && transform.position == Vector2.Distance(startMarker.position.x, startMarker.position.y)) 
+        {
+            Flip();
+        }
+        else if (facingRight == true && transform.position == Vector2.Distance(4,-2))
+        {
+            Flip();
+        }
+
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector2 Scaler = transform.localScale;
+        Scaler.x = Scaler.x * -1;
+        transform.localScale = Scaler;
+    }
+
+
+}
+
